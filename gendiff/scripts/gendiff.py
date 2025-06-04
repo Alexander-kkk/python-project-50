804af33f70fa33862740b219794c1f37c03207d4
Alexander <alexanderkuntsevo@gmail.com>
2025-06-05 00:55:18 +0300

    update gendiff script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,9 +1,11 @@
 from gendiff.cli import parse_args
+from gendiff.generate_diff import generate_diff
 
 
 def main():
     args = parse_args()
-    print(args)
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
 
 
 if __name__ == "__main__":
