e75d4acfa961f410e08d5012bd482086311827be
Alexander <alexanderkuntsevo@gmail.com>
2025-06-07 15:27:22 +0300

    update generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,6 +1,12 @@
 from gendiff.build_diff import build_diff
+from gendiff.formatter.plain import format_plain
 from gendiff.formatter.stylish import format_stylish
 from gendiff.parser import parse_data
+
+FORMATTERS = {
+    'stylish': format_stylish,
+    'plain': format_plain,
+}
 
 
 def generate_diff(file_path1, file_path2, format_name='stylish'):
@@ -8,8 +14,13 @@ def generate_diff(file_path1, file_path2, format_name='stylish'):
     data2 = parse_data(file_path2)
     diff = build_diff(data1, data2)
 
-    if format_name == 'stylish':
-        return f"{{\n{format_stylish(diff)}\n}}"
-    else:
+    formatter = FORMATTERS.get(format_name)
+    if not formatter:
         raise ValueError(f"Unknown format: {format_name}")
+    
+    result = formatter(diff)
+    
+    if format_name == 'stylish':
+        return f"{{\n{result}\n}}"
+    return result
     
